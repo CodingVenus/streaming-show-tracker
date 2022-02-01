57f3d981ff733f77d44d0f7973d65269b7c1f07b
Venus Teah <venus.teah@gmail.com>
2022-02-01 00:15:44 -0500

    Create mock get method

--- src/main/java/com/example/streaming_show_tracker/controller/ShowController.java
+++ src/main/java/com/example/streaming_show_tracker/controller/ShowController.java
@@ -1,13 +1,20 @@
 package com.example.streaming_show_tracker.controller;
 
+import com.example.streaming_show_tracker.model.Show;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/")
 public class ShowController {
 
-//    @GetMapping
+
+    @GetMapping
+    public List<Show> getShowList(){
+
+    }
 
 }
--- src/main/java/com/example/streaming_show_tracker/service/ShowService.java
+++ src/main/java/com/example/streaming_show_tracker/service/ShowService.java
@@ -1,8 +1,18 @@
 package com.example.streaming_show_tracker.service;
 
+import com.example.streaming_show_tracker.model.Show;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ShowService {
 
+    public List<Show> getShowList (){
+      return List.of(
+                new Show (
+                        1L, "Supernatural", "romance", "it's good", 2013, "watched"
+                )
+        );
+    }
 }
