f9d5c71f08f8f2751203fcf1a7ad4e19caa53d4d
Venus Teah <venus.teah@gmail.com>
2022-02-01 00:08:22 -0500

    Add annotations to controller class

--- src/main/java/com/example/streaming_show_tracker/controller/ShowController.java
+++ src/main/java/com/example/streaming_show_tracker/controller/ShowController.java
@@ -1,4 +1,11 @@
 package com.example.streaming_show_tracker.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/")
 public class ShowController {
+
+
 }
