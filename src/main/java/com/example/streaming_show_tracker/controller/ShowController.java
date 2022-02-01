606dbc18019e83ad029b6955e5e3a9431c7d8341
Venus Teah <venus.teah@gmail.com>
2022-02-01 00:10:25 -0500

    Create service package and service class for show

--- src/main/java/com/example/streaming_show_tracker/controller/ShowController.java
+++ src/main/java/com/example/streaming_show_tracker/controller/ShowController.java
@@ -1,5 +1,6 @@
 package com.example.streaming_show_tracker.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "api/")
 public class ShowController {
 
+//    @GetMapping
 
 }
