7a763835d5bfb10f84c5bfa1ceb36aa7d99b1861
Venus Teah <venus.teah@gmail.com>
2022-01-31 23:57:59 -0500

    Creating Java Spring Boot Project with Dependencies

---
+++ src/main/java/com/example/streaming_show_tracker/StreamingShowTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.streaming_show_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingShowTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingShowTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/streaming_show_tracker/StreamingShowTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.streaming_show_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingShowTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
