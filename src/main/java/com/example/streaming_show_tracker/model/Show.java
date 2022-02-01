2a800ce9b9d10cfb8464c84931f76ce206fcacab
Venus Teah <venus.teah@gmail.com>
2022-02-01 00:03:37 -0500

    Added model package and Show Class

---
+++ src/main/java/com/example/streaming_show_tracker/model/Show.java
@@ -0,0 +1,13 @@
+package com.example.streaming_show_tracker.model;
+
+
+public class Show {
+
+    private Long Id;
+    private String name;
+    private String genre;
+    private String description;
+    private Integer year;
+    private String watchStatus;
+
+}
