24ebbcfca714d8b67259032a48edde658232c8f4
Venus Teah <venus.teah@gmail.com>
2022-02-01 20:51:19 -0500

    Create User repository

---
+++ src/main/java/com/example/streaming_show_tracker/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.streaming_show_tracker.repository;
+
+import com.example.streaming_show_tracker.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
