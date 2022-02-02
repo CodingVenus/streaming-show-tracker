616b03a2ab34b1047cbe9e5a1a093db48fb14522
Venus Teah <venus.teah@gmail.com>
2022-02-01 20:24:18 -0500

    added constuctors, getters and setters for profile class

--- src/main/java/com/example/streaming_show_tracker/model/UserProfile.java
+++ src/main/java/com/example/streaming_show_tracker/model/UserProfile.java
@@ -14,4 +14,61 @@ public class UserProfile {
     private String lastName;
     private String profileDescription;
 
+
+    //CONSTRUCTORS
+    public UserProfile() {
+    }
+
+    public UserProfile(Long id, String firstName, String lastName, String profileDescription) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.profileDescription = profileDescription;
+    }
+
+
+    //GETTERS AND SETTERS
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getProfileDescription() {
+        return profileDescription;
+    }
+
+    public void setProfileDescription(String profileDescription) {
+        this.profileDescription = profileDescription;
+    }
+
+    //TO STRING
+
+    @Override
+    public String toString() {
+        return "UserProfile{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", profileDescription='" + profileDescription + '\'' +
+                '}';
+    }
 }
