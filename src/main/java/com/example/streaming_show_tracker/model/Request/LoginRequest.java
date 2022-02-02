15e7b771212849615f190abeb2cb1cdd97ded123
Venus Teah <venus.teah@gmail.com>
2022-02-01 23:01:56 -0500

    created login request file

---
+++ src/main/java/com/example/streaming_show_tracker/model/Request/LoginRequest.java
@@ -0,0 +1,16 @@
+package com.example.streaming_show_tracker.model.Request;
+
+public class LoginRequest {
+
+    private String emailAddress;
+    private String password;
+
+
+    public String getEmail() {
+        return emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
