461d5bf262f6f5132d90f52cc450138866dee7f7
Venus Teah <venus.teah@gmail.com>
2022-02-01 23:42:07 -0500

    add methods to UserRepository

--- src/main/java/com/example/streaming_show_tracker/repository/UserRepository.java
+++ src/main/java/com/example/streaming_show_tracker/repository/UserRepository.java
@@ -6,4 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
+    boolean existsByEmailAddress(String userEmailAddress);
+
+    User findUserByEmailAddress(String userEmailAddress);
+
 }
