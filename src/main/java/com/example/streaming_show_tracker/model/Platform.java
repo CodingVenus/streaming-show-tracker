ffe1b802cd3518135f92f4f93396f89329924f46
Venus Teah <venus.teah@gmail.com>
2022-02-01 19:28:48 -0500

    added constructors, getters and setters, and annotations for platform model

--- src/main/java/com/example/streaming_show_tracker/model/Platform.java
+++ src/main/java/com/example/streaming_show_tracker/model/Platform.java
@@ -1,8 +1,52 @@
 package com.example.streaming_show_tracker.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="platforms")
 public class Platform {
 
+    //FIELDS
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
+
+    //CONSTRUCTORS
+    public Platform() {
+    }
+
+    public Platform(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+    //GETTERS AND SETTERS
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    //TO STRING
+    @Override
+    public String toString() {
+        return "Platform{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
