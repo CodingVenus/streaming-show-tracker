40cf39ebca14ae8bdfc2c0269148e897032ec585
Venus Teah <venus.teah@gmail.com>
2022-02-01 21:11:12 -0500

    Create platform controller

---
+++ src/main/java/com/example/streaming_show_tracker/controller/PlatformController.java
@@ -0,0 +1,15 @@
+package com.example.streaming_show_tracker.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/platform")
+public class PlatformController {
+
+
+
+
+
+}
