2f8624261d85f3dc73609f982b0b1d5136400d99
Venus Teah <venus.teah@gmail.com>
2022-02-01 23:15:02 -0500

    Autowire jwtrequestfilter and add password encoder to security configurer

--- src/main/java/com/example/streaming_show_tracker/security/SecurityConfigurer.java
+++ src/main/java/com/example/streaming_show_tracker/security/SecurityConfigurer.java
@@ -1,8 +1,11 @@
 package com.example.streaming_show_tracker.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfigurer {
@@ -13,6 +16,16 @@ public class SecurityConfigurer {
     public SecurityConfigurer(MyUserDetailsService myUserDetailsService) {
         this.myUserDetailsService = myUserDetailsService;
     }
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
 
 
 }
