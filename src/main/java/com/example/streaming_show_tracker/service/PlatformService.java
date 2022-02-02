285fdb043317bb4cb744102425478ee8d9de274a
Venus Teah <venus.teah@gmail.com>
2022-02-02 10:30:05 -0500

    Created platform service get method

--- src/main/java/com/example/streaming_show_tracker/service/PlatformService.java
+++ src/main/java/com/example/streaming_show_tracker/service/PlatformService.java
@@ -1,8 +1,11 @@
 package com.example.streaming_show_tracker.service;
 
+import com.example.streaming_show_tracker.exceptions.InformationNotFoundException;
 import com.example.streaming_show_tracker.model.Platform;
 import com.example.streaming_show_tracker.repository.PlatformRepository;
+import com.example.streaming_show_tracker.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,9 +20,23 @@ public class PlatformService {
         this.platformRepository = platformRepository;
     }
 
-    //GETTER
-//    public List<Platform> getAllPlatforms() {
-//    }
-//
+    //GET METHOD
+    public List<Platform> getAllPlatforms() {
+
+        MyUserDetails userDetails = (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        System.out.println(userDetails.getUser().getId());
+
+        List<Platform> platform = platformRepository.findByUserId(userDetails.getUser().getId());
+
+        if (platform.isEmpty()) {
+            throw new InformationNotFoundException("No platform listings found for user id " + userDetails.getUser().getId());
+        } else {
+            return platform;
+        }
+    }
+
+    //
+
 
 }
