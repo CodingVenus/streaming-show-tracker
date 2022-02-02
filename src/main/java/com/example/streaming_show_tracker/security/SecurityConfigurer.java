01739bec033594d4e287ccc9a1c9f06b79fb5aba
Venus Teah <venus.teah@gmail.com>
2022-02-01 23:17:21 -0500

    Added  method for security configurer

--- src/main/java/com/example/streaming_show_tracker/security/SecurityConfigurer.java
+++ src/main/java/com/example/streaming_show_tracker/security/SecurityConfigurer.java
@@ -2,10 +2,13 @@ package com.example.streaming_show_tracker.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfigurer {
@@ -25,7 +28,18 @@ public class SecurityConfigurer {
         return new BCryptPasswordEncoder();
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
 
+        http.authorizeRequests().antMatchers(
+                        "/auth/users", "/auth/users/login", "/auth/users/register").permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().csrf().disable();
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
 
 
 }
