765f55ef07305c9f4ffb80d84c6d60db0a2f5022
Venus Teah <venus.teah@gmail.com>
2022-02-01 23:31:10 -0500

    Created JwtRequestFilter class, and autowired MyUserDetails and JWTUTils

--- src/main/java/com/example/streaming_show_tracker/security/JwtRequestFilter.java
+++ src/main/java/com/example/streaming_show_tracker/security/JwtRequestFilter.java
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-//@Component
-//public class JwtRequestFilter extends OncePerRequestFilter {
-//
-//
-//
-//}
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    private JWTUtils jwtUtils;
+
+}
