cde5eb3da333019a4e29a0ad85b7780b953196c7
Venus Teah <venus.teah@gmail.com>
2022-02-02 10:31:01 -0500

    Created findByUser Id method in Platform Repo class

--- src/main/java/com/example/streaming_show_tracker/repository/PlatformRepository.java
+++ src/main/java/com/example/streaming_show_tracker/repository/PlatformRepository.java
@@ -4,8 +4,11 @@ import com.example.streaming_show_tracker.model.Platform;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PlatformRepository extends JpaRepository<Platform, Long> {
 
+    List<Platform> findByUserId(Long userId);
 
 }
