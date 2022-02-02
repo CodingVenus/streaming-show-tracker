8fde92d3a0fd4d9455c3f171e6a71baca5c4db44
Venus Teah <venus.teah@gmail.com>
2022-02-02 12:52:05 -0500

    Removed s from user path and creating login and register post mapping

--- src/main/java/com/example/streaming_show_tracker/controller/UserController.java
+++ src/main/java/com/example/streaming_show_tracker/controller/UserController.java
@@ -1,14 +1,19 @@
 package com.example.streaming_show_tracker.controller;
 
+import com.example.streaming_show_tracker.model.Request.LoginRequest;
+import com.example.streaming_show_tracker.model.User;
 import com.example.streaming_show_tracker.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path ="/auth/users/")
+@RequestMapping(path ="/auth/user/")
 public class UserController {
 
     private UserService userService;
@@ -23,5 +28,13 @@ public class UserController {
         this.userDetailsService = userDetailsService;
     }
 
+    @PostMapping("/register")
+    public User createUser(@RequestBody User userObject) {
+        return userService.createUser(userObject);
+    }
 
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest) {
+        return userService.loginUser(loginRequest);
+    }
 }
--- src/main/java/com/example/streaming_show_tracker/security/SecurityConfigurer.java
+++ src/main/java/com/example/streaming_show_tracker/security/SecurityConfigurer.java
@@ -42,7 +42,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests().antMatchers(
-                        "/auth/users", "/auth/users/login", "/auth/users/register").permitAll()
+                        "/auth/user", "/auth/user/login", "/auth/user/register").permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
