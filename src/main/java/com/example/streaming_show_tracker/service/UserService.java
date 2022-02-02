b12236a9a1cbe3267c093be3a5837cffcf15be55
Venus Teah <venus.teah@gmail.com>
2022-02-01 22:56:45 -0500

    Add login user method to UserService

--- src/main/java/com/example/streaming_show_tracker/service/UserService.java
+++ src/main/java/com/example/streaming_show_tracker/service/UserService.java
@@ -3,18 +3,46 @@ package com.example.streaming_show_tracker.service;
 import com.example.streaming_show_tracker.repository.UserRepository;
 import com.example.streaming_show_tracker.security.JWTUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    @Autowired public void setUserRepository(UserRepository userRepository) {
+
+    private UserRepository userRepository;
+
+    @Autowired public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
     @Autowired private PasswordEncoder passwordEncoder;
+
     @Autowired private AuthenticationManager authenticationManager;
+
     @Autowired private UserDetailsService userDetailsService;
+
     @Autowired private JWTUtils jwtUtils;
+
+
+    //Authentication
+    public ResponseEntity<?> loginUser(LoginRequest loginRequest){
+
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(loginRequest.getEmail());
+
+        final String JWT = jwtUtils.generateToken(userDetails); //once it has been generated it can't be changed
+
+        return ResponseEntity.ok(new LoginResponse(JWT));
+
+    }
+
+
+
+
 }
